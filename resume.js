82ce9d5ac7ab8718efb24fd88396f2c4ac36f866
Harivasan10 <142417970+Harivasan10@users.noreply.github.com>
2023-08-25 17:06:44 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,15 @@
+var resume = {
+    my_Name: "Harivasan",
+    Email: "harivasan123@gmail.com",
+    Date_of_birth: "28.10-2002",
+    Age: '20',
+    Nationality: "Indian",
+    Languages_known: ["English", "Tamil"],
+    Marks: { "B.E": "CGPA 7.92", "HSC ": "87%", SSLC: "85%" },
+    Qualification: "B.E ECE",
+    Key_strength: ["creative", "self learning"],
+    Hobbies: ["Gaming", "listening music "],
+};
+
+var resume_JSON_Format = JSON.stringify(resume);
+console.log(resume_JSON_Format);
